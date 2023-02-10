1fc341a3946b1059fc582cc07275c636881cc812
kiki4701 <kiki4701@naver.com>
2023-02-10 09:48:53 +0900

    20230210

---
+++ fdfdf/src/fdfdf/_01.java
@@ -0,0 +1,20 @@
+package fdfdf;
+
+public class _01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte byteVal = 10;
+		char chVal = 'A';
+		int intVal = byteVal;
+		
+		System.out.println(intVal);
+		
+		intVal = chVal;
+		System.out.println(intVal);
+		
+		float fVal = intVal;
+		System.out.println(fVal);
+	}
+
+}
